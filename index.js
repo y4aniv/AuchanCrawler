002d2ff01e5a7f1922a37ceb579b714bbf86f785
Yaniv <120222659+y4aniv@users.noreply.github.com>
2023-06-19 15:05:21 +0000

    update 7 files and create 39 files

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
 var colors = require('colors');
 
 (async () => {
-    await require('./lib/setup')()
-    await require('./lib/setLogo')()
-    await require('./lib/getCookies')()
+    // await require('./lib/setup')()
+    // await require('./lib/setLogo')()
+    // await require('./lib/getCookies')()
     // await require('./lib/getCategories')()
-    global.categories = [
-        {
-          name: 'Cafés',
-          url: '/epicerie-sucree/cafes/ca-n0502',
-          uuid: '115b2aa0-6cb7-4f74-925e-5c4803c43ac4'
-        },
-        {
-          name: 'Petit déjeuner, thés, chocolats en poudre',
-          url: '/epicerie-sucree/petit-dejeuner-thes-chocolats-en-poudre/ca-n0503',
-          uuid: 'ee25490c-6918-492e-b1d1-1b83a587256f'
-        },
-        {
-          name: 'Biscuits, gâteaux',
-          url: '/epicerie-sucree/biscuits-gateaux/ca-n0505',
-          uuid: '8ffd5dc2-781c-487c-bf5a-3b9452a41c87'
-        },
-        {
-          name: 'Chocolats, confiseries',
-          url: '/epicerie-sucree/chocolats-confiseries/ca-n0507',
-          uuid: '8afc05d2-6903-4bdd-a341-7ef486e86cff'
-        },
-        {
-          name: 'Desserts, sucres, farines, diététique',
-          url: '/epicerie-sucree/desserts-sucres-farines-dietetique/ca-n0511',
-          uuid: '5663fc13-9aee-4aa2-98d2-cf5fcee80912'
-        },
-        {
-          name: 'Laits, petits-déjeuners de bébé',
-          url: '/bebe/laits-petits-dejeuners-de-bebe/ca-n0801',
-          uuid: '3fef5d20-59a4-4e25-a713-bd492265473d'
-        },
-      ]
-    await require('./lib/getProducts')()
+    // await require('./lib/getProducts')()
+    // await require('./lib/startServer')()
+    await require('./lib/startServerExpress')()
 })();
---
+++ web/src/App.js
@@ -0,0 +1,82 @@
+import './App.css';
+import "./ode-bootstrap-neo.css";
+import { Button, Input, FormControl } from "@ode-react-ui/components";
+
+function App() {
+  if (sessionStorage.getItem("NGROK") === null) {
+    return (
+      <div className="App">
+         <FormControl className="getNgrok">
+          <Input
+            placeholder="Enter the address displayed in your terminal"
+            size="md"
+            type="text"
+            id="ngrokURL"
+          />
+          <Button id="setNgrok" onClick={()=>{
+            let ngrok = document.getElementById("ngrokURL").value;
+            if (ngrok.indexOf("https://") === -1) {
+              ngrok = "https://" + ngrok;
+            }
+            if (ngrok.charAt(ngrok.length - 1) === "/") {
+              ngrok = ngrok.slice(0, -1);
+            }
+
+            fetch(ngrok + "/auchancrawler").then((res)=>{
+              if (res.status === 200) {
+                return res.json();
+              } else {
+                alert("The address provided appears to be incorrect. Please check it and try again.")
+              }
+            })
+            .then((data)=>{
+              if(data.message != "auchancrawler"){
+                alert("The address provided appears to be incorrect. Please check it and try again.")
+              }else{
+                sessionStorage.setItem("NGROK", ngrok);
+                window.location.reload();
+              }
+            })
+            .catch((err)=>{
+              alert("The address provided appears to be incorrect. Please check it and try again.")
+            })
+          }}>Launch the application</Button>
+        </FormControl>
+      </div>
+    )
+  } else {
+    return (
+      <div className="App">
+        {
+          setInterval(()=>{
+            fetch(sessionStorage.getItem("NGROK") + "/auchancrawler").then((res)=>{
+              return res.json();
+            }).then((data)=>{
+              if(data.message != "auchancrawler"){
+                sessionStorage.clear();
+                window.location.reload();
+              }
+            }).catch((err)=>{
+              sessionStorage.clear();
+              window.location.reload();
+            })
+          }, 1000)
+        }
+        <FormControl className="App-SearchForm">
+          <Input
+            placeholder="Search for a product, brand or category..."
+            size="md"
+            type="text"
+          />
+        </FormControl>
+        <div className="Results">
+          <div id="Results-Products"></div>
+          <div id="Results-Brands"></div>
+          <div id="Results-Categories"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
